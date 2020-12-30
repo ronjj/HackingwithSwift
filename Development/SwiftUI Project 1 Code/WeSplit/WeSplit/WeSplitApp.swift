c5fe75643fb48ab4cc26fb32d248d6718ff0fbee
Ronald Jabouin <37760008+ronjj@users.noreply.github.com>
2020-12-29 22:42:31 -0500

    Add files via upload
    Projects 1 and 2 from HackingWithSwift 
    
    * Project 2 was modified to have my own features. Added the ability to see the score and styled it. Also added the feature where a point is added for getting a question right and a point is lost for getting the question wrong

---
+++ Development/SwiftUI Project 1 Code/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Ronald Jabouin on 12/28/20.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
