059ab65e5a0d2b4452af40b797122d1ba1c3b11e
Ronald Jabouin <37760008+ronjj@users.noreply.github.com>
2021-01-03 20:18:23 -0500

    Add files via upload
    Added Project 3 and 5
    
    Project 5 I built a word scrambler. Learned about the List view which is similar to form view but does not allow users to import data. Also learned about reading data from outside sources and then turning that data in arrays which I can use.

---
+++ Development/HWS Project 3/ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Ronald Jabouin on 12/30/20.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
