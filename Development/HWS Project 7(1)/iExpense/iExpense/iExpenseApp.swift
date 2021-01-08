1d3051c9fa16ac0674fef32a729662bb4a4576e7
Ronald Jabouin <37760008+ronjj@users.noreply.github.com>
2021-01-07 22:34:30 -0500

    Add files via upload
    Learned many things in this tutorial. Learned about the difference of using structs vs classes in some instances. This is good for using and updating the same data across views. Created another view which passed data back into the original view. Learned how to add and delete rows within a list. Learned about saving the data inputted by the user when the app is restarted. Finally, learned how to present that data using JSON.

---
+++ Development/HWS Project 7(1)/iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Ronald Jabouin on 1/7/21.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
